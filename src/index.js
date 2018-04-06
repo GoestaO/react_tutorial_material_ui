16d3c6f8c318000674910d3f00152d5a11414be3
Gösta Ostendorf <gostendorf@zalando.de>
2018-04-06 16:53:59 +0200

    Custom menu

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import AppBar from 'material-ui/AppBar';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 
 class MyAppBar extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-        <AppBar
+        <AppBar className="appBar"
           title="My Application"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
           onTitleClick={() => this.handleClick()}
+          iconElementLeft={<CustomMenu/>}
+          iconElementRight={<FlatButton label="Save" />}
         />
       </MuiThemeProvider>
     );
@@ -26,8 +36,29 @@ class MyAppBar extends React.Component {
     alert('onClick triggered on the title component');
   }
 
-  const
+}
 
+
+
+
+class CustomMenu extends React.Component {
+  render () {
+    return (
+      <IconMenu
+        iconButtonElement={
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem primaryText="Refresh" />
+        <MenuItem primaryText="Help" />
+        <MenuItem primaryText="Sign out" />
+      </IconMenu>
+  );
+  }
 }
 
 
